b5956e3df851a5b3a90cd44202358f9dccb267ec
JosielFaleiros <josiel.2015@alunos.utfpr.edu.br>
2018-04-04 18:50:10 -0300

    joined Travado and Destravado features

--- src/test/java/cofre/CofrePMSteps.java
+++ src/test/java/cofre/CofrePMSteps.java
@@ -17,18 +17,18 @@ import static org.mockito.Mockito.when;
  *
  * @author JosielFaleiros
  */
-public class TravadoCofrePMSteps extends CofrePMTest{
+public class CofrePMSteps extends CofrePMTest {
 
     @Given("^que temos uma instancia de CofrePM com SensorPorta e uma Memoria$")
     public void que_temos_uma_instancia_de_CofrePM_com_SensorPorta_e_uma_Memoria() throws Exception {
         sensorMock = mock(SensorPorta.class);
         memoriaMock = mock(Memoria.class);
-        cofrePM = new CofrePM(sensorMock, memoriaMock);    
+        cofrePM = new CofrePM(sensorMock, memoriaMock);
 
         when(sensorMock.fechada())
                 .thenReturn(Boolean.TRUE);
         when(sensorMock.travada())
-                .thenReturn(Boolean.TRUE);        
+                .thenReturn(Boolean.TRUE);
     }
 
     @Given("^eu digitei a senha invalida$")
@@ -41,6 +41,16 @@ public class TravadoCofrePMSteps extends CofrePMTest{
         cofrePM.pressButton(4);
         cofrePM.pressButton(6);
         cofrePM.pressButton(6);
+    }
+
+    @Given("^eu digitei uma senha com a porta aberta$")
+    public void eu_digitei_uma_senha_com_a_porta_aberta() throws Exception {
+        when(sensorMock.fechada())
+                .thenReturn(Boolean.FALSE);
+        
+        for(int botao = 0; botao <=9; botao++) {
+            cofrePM.pressButton(botao);
+        }
     }
 
     @When("^eu apertei ok$")
--- src/test/resources/cofre/CofrePM.feature
+++ src/test/resources/cofre/CofrePM.feature
@@ -1,13 +1,20 @@
 # language: en
 # Source: http://github.com/aslakhellesoy/cucumber/blob/master/examples/i18n/en/features/addition.feature
 # Updated: Tue May 25 15:51:43 +0200 2010
-Feature: TravadoCofrePM
+Feature: CofrePM
   Como um usuario
   eu quero digitar a senha
   e saber se ela abre ou não o cofre
+  ou poder travar o cofre quando ele está destravado
 
   Scenario: Testar se senha abre cofre
     Given que temos uma instancia de CofrePM com SensorPorta e uma Memoria
     And eu digitei a senha invalida
     When eu apertei ok
     Then a mensagem do display deve ser "senha errada. Tente novamente"
+
+  Scenario: Testar travar cofre colocando senha com porta aberta
+    Given que temos uma instancia de CofrePM com SensorPorta e uma Memoria
+    And eu digitei uma senha com a porta aberta
+    When eu apertei ok
+    Then a mensagem do display deve ser "Feche a porta antes de digitar a senha"
